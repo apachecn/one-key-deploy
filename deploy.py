b67767db6ac1df9f73fd63d1f2be96cc6fa1fe37
wizardforcel <562826179@qq.com>
2022-01-15 14:58:22 +0800

    2022-01-15 14:58:22

--- deploy.py
+++ deploy.py
@@ -54,6 +54,50 @@ def rmtree_safe(dir):
     if path.exists(dir):
         shutil.rmtree(dir)
 
+def deploy_home():
+    config = json.loads(
+        open(d('config.json'), encoding='utf-8').read())
+    # 释放配置文件
+    data_dir = config['dataDir']
+    mkdirs_safe(data_dir)
+    conf_dir = path.join(data_dir, 'conf')
+    mkdirs_safe(conf_dir)
+    rsrc_dir = path.join(data_dir, 'html')
+    mkdirs_safe(rsrc_dir)
+    log_dir = path.join(data_dir, 'log')
+    mkdirs_safe(log_dir)
+    ssl_dir = config['sslDir']
+    
+    shutil.copy(
+        d('asset/conf/' + config['conf'] + '.conf'),
+        path.join(conf_dir, 'default.conf'),
+    )
+    
+    # 启动 Nginx
+    name, port, sec_port = config['name'], config['port'], config['secPort']
+    print(f'name: {name}, repo: nginx, port: {port}, secPort: {sec_port}')
+    kill_port(port)
+    kill_port(sec_port)
+    subp.Popen(f'docker rm -f {name}', shell=True).communicate()
+    subp.Popen(f'docker pull nginx', shell=True).communicate()
+    args = '\x20'.join([
+        # 后台运行
+        'docker run -tid',
+        # 设置容器名称
+        f'--name {name}',
+        # 绑定端口
+        f'-p {port}:80',
+        f'-p {sec_port}:443',
+        # 绑定配置、资源和日志目录
+        f'-v "{conf_dir}:{DOCKER_NGINX_CONF}"',
+        f'-v "{rsrc_dir}:{DOCKER_NGINX_RSRC}"',
+        f'-v "{log_dir}:{DOCKER_NGINX_LOG}"',
+        f'-v "{ssl_dir}:{DOCKER_NGINX_SSL}"',
+        # 镜像名称
+        'nginx',
+    ])
+    subp.Popen(args, shell=True).communicate()
+
 def deploy_doc():
     config = json.loads(
         open(d('config.json'), encoding='utf-8').read())
@@ -129,7 +173,6 @@ def deploy_doc():
         f'-v "{conf_dir}:{DOCKER_NGINX_CONF}"',
         f'-v "{rsrc_dir}:{DOCKER_NGINX_RSRC}"',
         f'-v "{log_dir}:{DOCKER_NGINX_LOG}"',
-        f'-v "{ssl_dir}:{DOCKER_NGINX_SSL}"',
         # 镜像名称
         'nginx',
     ])
